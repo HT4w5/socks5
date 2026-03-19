714ba5a5263118524d07e4dafedaee1b1902cc65
HT4w5 <ht4w5x@gmail.com>
2026-03-20 01:16:36 +0800

    fix(server): send IPv4 0.0.0.0 on non-success replies

--- pkg/server/utils.go
+++ pkg/server/utils.go
@@ -1,8 +1,26 @@
 package server
 
-import "io"
+import (
+	"io"
+	"net"
+	"net/netip"
+
+	"github.com/HT4w5/socks5/pkg/payload"
+)
 
 func copy(dst io.Writer, src io.Reader, errCh chan error) {
 	_, err := io.Copy(dst, src)
 	errCh <- err
+}
+
+// Send socks reply with rep (failure only)
+func (s *Server) sendFailureReply(conn net.Conn, rep uint8) {
+	s.logger.Warnf("sending reply: %s", payload.ReplyReason(rep))
+	r := payload.NewReply(
+		payload.ReplyWithRep(rep),
+		payload.ReplyWithIP(netip.IPv4Unspecified()),
+	)
+	if err := r.Write(conn); err != nil {
+		s.logger.Errorf("failed to write reply: %v", err)
+	}
 }
